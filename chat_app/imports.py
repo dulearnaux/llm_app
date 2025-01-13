6467bf00a2c2243b9aac18a812e3093b187c55fa
dushan <dushan4@gmail.com>
2025-01-13 12:40:09 +1100

    created separate dir for chat app

---
+++ chat_app/imports.py
@@ -0,0 +1,7 @@
+"""This file lists imports not correctly processed by pipreqs.
+
+This file isn't run, but ensures the requirements.txt file is generated correctly.
+"""
+
+import sklearn
+from bs4 import BeautifulSoup
