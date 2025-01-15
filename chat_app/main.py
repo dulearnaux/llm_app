f88a19535ceec31b9e6612ffc8edb61ccc85d085
dushan <dushan4@gmail.com>
2025-01-15 13:07:11 +1100

    Created chat interface with history.

--- chat_app/main.py
+++ chat_app/main.py
@@ -93,7 +93,25 @@ if __name__ == '__main__':
     st.sidebar.subheader('RAG docs are:')
     st.sidebar.write(f'{tmp}')
 
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
     rag_app = RAGApp(retriever, rag_chain)
-    q = st.text_area('Write your query here', 'what is prompt engineering?')
-    a = rag_app.run(q)
-    st.write(a)
+    if prompt := st.chat_input("Write your question here?"):
+        # Display user message in chat message container
+        st.chat_message("user").markdown(prompt)
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+        response = rag_app.run(prompt)
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            st.markdown(response)
+        # Add assistant response to chat history
+        st.session_state.messages.append(
+            {"role": "assistant", "content": response})
